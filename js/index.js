1ee832d9a113cd360d56ad7168d8a7d7afb2e73f
ximinz <ximinz@DESKTOP-CU0LKDT.localdomain>
2020-04-03 23:39:54 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+
+	document.getElementById("showQuestion1").onclick = function() { 
+		document.getElementById("question1").innerHTML = "Dear Robert, You are inherited Edward's manor. Do you want to accept it?"
+	} 
+}
