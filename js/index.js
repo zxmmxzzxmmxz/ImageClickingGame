1b3198622d8bc0be8feef16a247b3157d5255331
ximinz <ximinz@DESKTOP-CU0LKDT.localdomain>
2020-04-05 02:43:48 -0700

    scene1 and scene2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
 window.onload = function(){
 
-	document.getElementById("showQuestion1").onclick = function() { 
-		document.getElementById("question1").innerHTML = "Dear Robert, You are inherited Edward's manor. Do you want to accept it?"
-		document.getElementById("showQuestion1").style.display = "none";
-		document.getElementById("click").style.display = "none";
-		document.getElementById("button1").style.display="block";
+	document.getElementById("toScene1").onclick = function() { 
+		window.location.href = "scene1.html";
 	} 
 
-	document.getElementById("button1span").onclick = function() {
-		window.location.href = "scene1.html";
-	}
 
 }
---
+++ js/scene1.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+
+	document.getElementById("click").onclick = function() { 
+		window.location.href = "scene2.html";
+	} 
+
+
+}
